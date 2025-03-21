43c7e730e1d9525d65dff925b9636cb4ad74ca3b
andy.lin <andy.lin@innovue.ltd>
2025-03-21 23:12:06 +0800

    immer

---
+++ bun/immer/10.vue.ts
@@ -0,0 +1,27 @@
+﻿// @ts-nocheck
+
+import { produce } from "immer";
+
+export function useImmutable<T extends object>(baseState: T) {
+    const state = shallowRef(baseState);
+    const setState = (updater: (draft: T) => void) => {
+        state.value = produce(state.value, updater);
+    }
+    return [state, setState];
+}
+
+const [product, setProduct] = useImmutable({
+    name: "robot",
+    quantity: 100,
+    history: []
+});
+
+setProduct((draft) => {
+    draft.quantity -= 10;
+    draft.history.push(10);
+});
+
+
+
+
+function shallowRef() {}
---
+++ bun/immer/11.undo-with-immer.ts
@@ -0,0 +1,38 @@
+﻿// @ts-nocheck
+
+import { applyPatches, Patch, produce } from "immer";
+
+export function useImmutable<T extends object>(baseState: T) {
+    const inversePatchesHistory: Patch[] = [];
+
+    const state = shallowRef(baseState);
+    const setState = (updater: (draft: T) => void) => {
+        state.value = produce(state.value, updater, (_, inversePatches) => {
+            inversePatchesHistory.push(...inversePatches);
+        });
+    }
+    const undo = () => {
+        const inversePatches = inversePatchesHistory.pop();
+        if (inversePatches) {
+            state.value = applyPatches(state.value, inversePatches);
+        }
+    }
+    const reset = () => {
+        state.value = baseState;
+        inversePatchesHistory.length = 0;
+    }
+    return [state, setState, undo, reset];
+}
+
+const [product, setProduct, undo, reset] = useImmutable({
+    name: "robot",
+    quantity: 100,
+    history: []
+});
+
+setProduct((draft) => {
+    draft.quantity -= 10;
+    draft.history.push(10);
+});
+
+function shallowRef() {}
